8b30b21f03c9d3091754589a157dd853e4f8f3db
vielka <vielka.pradorojas.2022@student.lhschools.org>
2019-09-20 09:38:59 -0400

    Coded the intro to BMI

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,29 @@
+package com.LickingHeights;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	   Scanner keyboard;
+	   keyboard = new Scanner(System.in);
+
+	   Double inch;
+	   Double pound;
+	   int imp;
+	   System.out.println("Hello ");
+	   System.out.println("Answer these questions to calculate your body mass index.");
+	   System.out.println("After asnwering questions your body mass index will be calculated.");
+	   
+
+
+
+
+
+
+
+
+
+
+    }
+}
